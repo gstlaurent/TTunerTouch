41a294be409e99b7551096f51cc2120b31164334
Graham St-Laurent <gstlaurent@gmail.com>
2018-02-05 01:30:45 -0800

    Changed ratios from numerators and denominators, or doubles, into Rational data type.

--- app/src/main/java/com/thefourthspecies/ttunertouch/Temperament.kt
+++ app/src/main/java/com/thefourthspecies/ttunertouch/Temperament.kt
@@ -1,12 +1,14 @@
 package com.thefourthspecies.ttunertouch
 
-enum class Comma(ratio: Double, val symbol: String) {
-    PYTHAGOREAN(531441.0/524288.0, "P"), // (12*P5)/(7*P8) = ((3/2)^12)/(2^7) = (3^12)/(2^19)
-    SYNTONIC(81.0/80.0, "S"), // (4*P5)/(1*M3+2*P8) = (3/2)^4/(5/4 * 2^2) = (3^4)/((2^4)*5)
-    ENHARMONIC(128.0/125.0, "E"), // P8/3*M3 = (2^12)/((5/4)^3) = (2^7)/(5^3)
-    PURE(1.0, "=")
+import android.util.Rational
+
+enum class Comma(ratio: Rational, val symbol: String) {
+    PYTHAGOREAN(Rational(531441, 524288), "P"), // (12*P5)/(7*P8) = ((3/2)^12)/(2^7) = (3^12)/(2^19)
+    SYNTONIC(Rational(81, 80), "S"), // (4*P5)/(1*M3+2*P8) = (3/2)^4/(5/4 * 2^2) = (3^4)/((2^4)*5)
+    ENHARMONIC(Rational(128, 125), "E"), // P8/3*M3 = (2^12)/((5/4)^3) = (2^7)/(5^3)
+    PURE(Rational(1,1), "=")
 }
-data class Temper(val numerator: Int, val denominator: Int, val comma: Comma)
+data class Temper(val magnitude: Rational, val comma: Comma)
 
 /**
  * Two Relationships are considered equal if they contain the same notes, whether they are note2 or note1,
